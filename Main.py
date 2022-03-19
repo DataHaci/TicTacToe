88d29d3533f0f453c208fe88b432dca2e2e1dc76
Haki C <yaki89@hotmail.de>
2022-03-20 00:23:19 +0100

    Defined winning conditions

--- Main.py
+++ Main.py
@@ -1,2 +1,16 @@
 def mainfunc():
     'read thoughts.txt'
+    '3x3 Matrix is like:' \
+    '123' \
+    '456' \
+    '789'
+    wc = WinningCondition
+
+    wc1 = [1, 2, 3]
+    wc2 = [4, 5, 6]
+    wc3 = [7, 8, 9]
+    wc4 = [1, 4, 7]
+    wc5 = [2, 5, 8]
+    wc6 = [3, 6, 9]
+    wc7 = [1, 5, 9]
+    wc8 = [3, 5, 6]
