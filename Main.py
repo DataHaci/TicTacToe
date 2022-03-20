0a5c2e1cd8052d928b1343eebe71cf885d85709e
Haki C <yaki89@hotmail.de>
2022-03-20 16:02:28 +0100

    Defined new winning conditions

--- Main.py
+++ Main.py
@@ -1,29 +1,28 @@
-def mainfunc():
-    'read thoughts.txt'
-    '3x3 Matrix is like:' \
-    '123' \
-    '456' \
-    '789'
 
-    'wc = WinningCondition
-    wc1 = [1, 2, 3]
-    wc2 = [4, 5, 6]
-    wc3 = [7, 8, 9]
-    wc4 = [1, 4, 7]
-    wc5 = [2, 5, 8]
-    wc6 = [3, 6, 9]
-    wc7 = [1, 5, 9]
-    wc8 = [3, 5, 7]
+'read thoughts.txt'
+'3x3 Matrix is like:' \
+'123' \
+'456' \
+'789'
 
-    wcn1 = [1, 1, 1, 0, 0, 0, 0, 0, 0]
-    wcn2 = [0, 0, 0, 1, 1, 1, 0, 0, 0]
-    wcn3 = [0, 0, 0, 0, 0, 0, 1, 1, 1]
-    wcn4 = [1, 0, 0, 1, 0, 0, 7, 0, 0]
-    wcn5 = [0, 1, 0, 0, 1, 0, 0, 1, 0]
-    wcn6 = [0, 0, 1, 0, 0, 1, 0, 0, 1]
-    wcn7 = [1, 0, 0, 0, 1, 0, 0, 0, 1]
-    wcn8 = [0, 0, 1, 0, 1, 0, 1, 0, 0]
+import numpy as np
 
-    player1 = [0, 0, 0, 0, 0, 0, 0, 0, 0]
-    player2 = [0, 0, 0, 0, 0, 0, 0, 0, 0]
+#wcx = WinningCondition
+wc1 = [1, 2, 3]
+wc2 = [4, 5, 6]
+wc3 = [7, 8, 9]
+wc4 = [1, 4, 7]
+wc5 = [2, 5, 8]
+wc6 = [3, 6, 9]
+wc7 = [1, 5, 9]
+wc8 = [3, 5, 7]
+
+player1 = [0, 0, 0, 0, 0, 0, 0, 0, 0]
+player2 = [0, 0, 0, 0, 0, 0, 0, 0, 0]
+
+'If player1 or player2 contains all three numbers of wcx in one of the wcx, then win condition'
+
+WCPlayer1 = (wc1[0] in player1, wc1[1] in player1, wc1[2] in player1)
+print(WCPlayer1)
+
 
