fe775641e9546f24dd7176143ebe49dfd4a6d395
Haki C <yaki89@hotmail.de>
2022-03-20 16:18:44 +0100

    Look in Element in list

--- Main.py
+++ Main.py
@@ -5,8 +5,6 @@
 '456' \
 '789'
 
-import numpy as np
-
 #wcx = WinConditions
 WinConList = ([1, 2, 3], [4, 5, 6], [7, 8, 9], [1, 4, 7], [2, 5, 8], [3, 6, 9], [1, 5, 9], [3, 5, 7])
 
@@ -15,11 +13,13 @@ player2 = [0, 0, 0, 0, 0, 0, 0, 0, 0]
 
 'If player1 or player2 contains all three numbers of wcx in one of the wcx, then win condition'
 
-WCwin = (wc1[0] in player1, wc1[1] in player1, wc1[2] in player1)
+n = 0
+WCwin = (WinConList[n][0] in player1, WinConList[n][1] in player1, WinConList[n][2] in player1)
 Alle = all(WCwin)
 print(Alle)
 
 'Iterate trough all wcx'
 'wc + n'
+
 
 
