82ba073c5d4d30e1afa8f43ff20e734536bc6dc1
Haki C <yaki89@hotmail.de>
2022-03-20 16:47:06 +0100

    Iterate WinConList

--- Main.py
+++ Main.py
@@ -1,30 +1,26 @@
-
 'read thoughts.txt'
 '3x3 Matrix is like:' \
 '123' \
 '456' \
 '789'
 
-#wcx = WinConditions
+'List of Winning Coordinates'
 WinConList = ([1, 2, 3], [4, 5, 6], [7, 8, 9], [1, 4, 7], [2, 5, 8], [3, 6, 9], [1, 5, 9], [3, 5, 7])
 
 player1 = [1, 2, 3, 0, 0, 0, 0, 0, 0]
 player2 = [0, 0, 0, 0, 0, 0, 0, 0, 0]
 
-'If player1 or player2 contains all three numbers of wcx in one of the wcx, then win condition'
-
+'If player1 or player2 contains all three numbers of WinConList in one of the WinConList, then win condition'
 def proofWinConList():
     WCwin = (WinConList[n][0] in player1, WinConList[n][1] in player1, WinConList[n][2] in player1)
     Alle = all(WCwin)
     print(Alle)
 
 'Iterate trough all WinConList'
-
 n = 0
 while n < 8:
   proofWinConList()
   n += 1
-
 
 
 
