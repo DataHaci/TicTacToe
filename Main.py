7eca09baac495bc7e144e89a4d4a93f7642f9dde
Haki C <yaki89@hotmail.de>
2022-03-20 23:42:07 +0100

    Define Iteration as function Give variable n to WinConList

--- Main.py
+++ Main.py
@@ -5,22 +5,28 @@
 '789'
 
 'List of Winning Coordinates'
-WinConList = ([1, 2, 3], [4, 5, 6], [7, 8, 9], [1, 4, 7], [2, 5, 8], [3, 6, 9], [1, 5, 9], [3, 5, 7])
+'If list would be bigger, I suggest to use dictionaries/hashmaps'
+WinConList = [[1, 2, 3], [4, 5, 6], [7, 8, 9], [1, 4, 7], [2, 5, 8], [3, 6, 9], [1, 5, 9], [3, 5, 7]]
 
 player1 = [1, 2, 3, 0, 0, 0, 0, 0, 0]
 player2 = [0, 0, 0, 0, 0, 0, 0, 0, 0]
 
 'If player1 or player2 contains all three numbers of WinConList in one of the WinConList, then win condition'
-def proofWinConList():
+'n element in WinConList'
+def proofWinConList(n):
     WCwin = (WinConList[n][0] in player1, WinConList[n][1] in player1, WinConList[n][2] in player1)
     Alle = all(WCwin)
     print(Alle)
 
 'Iterate trough all WinConList'
-n = 0
-while n < 8:
-  proofWinConList()
-  n += 1
+def IterateWCL():
+    n = 0
+    while n < 8:
+        proofWinConList(n)
+        n += 1
+
+IterateWCL()
+
 
 
 
