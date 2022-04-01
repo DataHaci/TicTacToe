d57844db26b3627e8c950cee7d627b5a32ba9866
Haki C <yaki89@hotmail.de>
2022-04-01 02:06:21 +0200

    Naming

--- Sketchbook.py
+++ Sketchbook.py
@@ -3,8 +3,13 @@ WinningConditions = [[1, 2, 3], [4, 5, 6], [7, 8, 9], [1, 4, 7], [2, 5, 8], [3, 
 player1 = [0, 0, 0, 0, 0, 0, 0, 0, 0]
 player2 = [0, 0, 0, 0, 0, 0, 0, 0, 0]
 
-#for x in range(9):
-Position = input("Welches Feld? ")
-Position = int(Position)
-player1[Position-1] = Position
-print(player1)
+for x in range(5):
+    Position = input("Which field? ")
+    Position = int(Position)
+    player1[Position-1] = Position
+    print("player1 ", player1)
+
+    Position = input("Which field? ")
+    Position = int(Position)
+    player2[Position-1] = Position
+    print("player2 ", player2)
