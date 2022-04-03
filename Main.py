40934f4a2edcec113355f52b2d8bc06a1f994f7e
Haki C <yaki89@hotmail.de>
2022-04-03 16:48:08 +0200

    Updated Congratulations who has won

--- Main.py
+++ Main.py
@@ -4,7 +4,10 @@
 '456' \
 '789'
 
-WinningConditions = [[1, 2, 3], [4, 5, 6], [7, 8, 9], [1, 4, 7], [2, 5, 8], [3, 6, 9], [1, 5, 9], [3, 5, 7]]
+#Test if field is already used
+#clean code
+
+WinConditions = [[1, 2, 3], [4, 5, 6], [7, 8, 9], [1, 4, 7], [2, 5, 8], [3, 6, 9], [1, 5, 9], [3, 5, 7]]
 
 Field = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
 
@@ -23,32 +26,36 @@ def WriteInPlayer():
         Field[Position - 1] = "X"
 
         FieldVisualization()
-        IterateWinConditionsP1()
+        CheckIfP1won()
 
         Position = int(input("Which field Player 2? "))
         player2[Position - 1] = Position
         Field[Position - 1] = "O"
 
         FieldVisualization()
-        IterateWinConditionsP2()
+        CheckIfP2won()
 
-def IterateWinConditionsP1():
-    for x in WinningConditions:
+def CheckIfP1won():
+    for x in WinConditions:
         WCwin = (x[0] in player1, x[1] in player1, x[2] in player1)
         Alle = all(WCwin)
         if Alle:
-            print("Player 1 has won!")
+            print("""
+            #################################
+            CONGRATULATION - PLAYER1 HAS WON!
+            #################################""")
             quit()
 
-def IterateWinConditionsP2():
-    for x in WinningConditions:
+def CheckIfP2won():
+    for x in WinConditions:
         WCwin = (x[0] in player2, x[1] in player2, x[2] in player2)
         Alle = all(WCwin)
         if Alle:
-            print("Player 2 has won!")
+            print("""
+            #################################
+            CONGRATULATION - PLAYER1 HAS WON!
+            #################################""")
             quit()
 
 WriteInPlayer()
 
-#Prüfung ob feld belegt einbauen, z.B. ist Position ungleich 0
-#clean code
